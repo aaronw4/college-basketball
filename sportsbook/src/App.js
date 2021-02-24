9fe1dea86763afd01445d7ff9c27d63e64cafe78
Aaron Williams <aaronrw4@yahoo.com>
2021-02-23 20:08:38 -0800

    Created new routes and link

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {Route, Link} from 'react-router-dom';
 import GameLines from './Components/gameLines';
+import Tests from "./Components/tests";
 
 function App() {
   return (
     <div className="App">
-      <GameLines/>
+      <Route exact path='/'>        
+        <GameLines/>
+        <Link onClick={Tests} to='/tests'>
+          <h3>Tests</h3>
+        </Link>
+      </Route>
+      <Route path='/tests'>
+        <Tests/>
+      </Route>
     </div>
   );
 }
