207b2bec141ac580c5b3390acba2991f2921ebc3
Aaron Williams <aaronrw4@yahoo.com>
2021-02-24 21:31:05 -0800

    Created projectedScores for tests

---
+++ sportsbook/src/Components/projectedScores.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {findTeam} from './findTeam';
+
+function projectedScores (away, home){
+    // home/away undefined error means findTeam() cant find the team name in stats.json
+
+    let data = require("../stats.json")
+    let adjTvalues = data.map(value => Number(value.AdjT))
+    let adjTTotal = adjTvalues.reduce((total, amount) => total + amount)
+    let adjTAve = adjTTotal / adjTvalues.length
+    let possessions = (away.AdjT/adjTAve)*home.AdjT
+
+    let adjOvalues = data.map(value => Number(value.AdjO))
+    let adjOtotals = adjOvalues.reduce((total, amount) => total + amount)
+    let adjOave = adjOtotals / adjOvalues.length
+    let scoreAway = (away.AdjO/adjOave)*home.AdjD*(possessions/100)
+    let scoreHome = (home.AdjO/adjOave)*away.AdjD*(possessions/100)
+
+    let spreadHome = (scoreAway - scoreHome).toFixed(1)
+    let spreadAway = (scoreHome - scoreAway).toFixed(1)
+    let total = (scoreAway + scoreHome).toFixed(1)
+
+    return ([{
+        away: away.name,
+        home: home.name,
+        scoreAway: scoreAway.toFixed(1), 
+        scoreHome: scoreHome.toFixed(1),
+        spreadAway: spreadAway,
+        spreadHome: spreadHome,
+        total: total
+    }])
+}
+
+const ProjectedLines = (props) => {
+    let awayStats = findTeam(props.away);
+    let homeStats = findTeam(props.home);
+    let projectedStats = projectedScores(awayStats[0], homeStats[0]);
+    
+    return (
+        <div className='fgOpening'>
+            <h5>Full Game Projected</h5>
+            <p>Away: {projectedStats[0].spreadAway}</p>
+            <p>Home: {projectedStats[0].spreadHome}</p>
+            <br/>
+            <p>Away: {projectedStats[0].scoreAway}</p>
+            <p>Home: {projectedStats[0].scoreHome}</p>
+            <br/>
+            <p>Over {projectedStats[0].total}</p>
+            <p>Under {projectedStats[0].total}</p>
+        </div>
+    )
+}
+
+export default ProjectedLines
