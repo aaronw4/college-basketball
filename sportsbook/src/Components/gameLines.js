8163604e3f4cf7b3db42a8e65d1553dbf859a046
Aaron Williams <aaronrw4@yahoo.com>
2021-03-01 10:10:09 -0800

    Pulled out sign function as its own component

--- sportsbook/src/Components/gameLines.js
+++ sportsbook/src/Components/gameLines.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import {MidPoint} from '../Functions/midpoint';
 import ProjectedLines from './projectedLines';
+import ProjectedLinesSOS from './projectedSOSscores';
+import FullGameMP from './fullGameMP';
+import FirstHalfMP from './firstHalfMP'
 
 const GameLines = () => {
     let data = require('../odds.json')
-
-    function sign(value, otherValue) {
-        if (Number(value) < Number(otherValue)) {
-            return('-')
-        } else {
-            return('+')
-        }
-    }
 
     return (
         <div>
@@ -33,66 +27,31 @@ const GameLines = () => {
                             away = {game.teams.away}
                             home = {game.teams.home}
                         />
-                        <div className='fgMidpoint'>
-                            <h5>Full Game Midpoints</h5>
-                            <p>
-                                Away: {game.full_game.spread.away_spread}
-                                ({sign(game.full_game.spread.away_odds, game.full_game.spread.home_odds)}
-                                {MidPoint(game.full_game.spread.away_odds, game.full_game.spread.home_odds)})
-                            </p>
-                            <p>
-                                Home: {game.full_game.spread.home_spread}
-                                ({sign(game.full_game.spread.home_odds, game.full_game.spread.away_odds)}
-                                {MidPoint(game.full_game.spread.away_odds, game.full_game.spread.home_odds)})</p>
+                        <ProjectedLinesSOS
+                            away = {game.teams.away}
+                            home = {game.teams.home}
+                        />
+                        <div className='fgOpening'>
+                            <h5>Full Game Opening</h5>
+                            <p>Away: {game.full_game.spread.away_spread}({game.full_game.spread.away_odds})</p>
+                            <p>Home: {game.full_game.spread.home_spread}({game.full_game.spread.home_odds})</p>
                             <br/>
-                            <p>
-                                Away: {sign(game.full_game.moneyline.away, game.full_game.moneyline.home)}
-                                {MidPoint(game.full_game.moneyline.away, game.full_game.moneyline.home)}</p>
-                            <p>
-                                Home: {sign(game.full_game.moneyline.home, game.full_game.moneyline.away)}
-                                {MidPoint(game.full_game.moneyline.away, game.full_game.moneyline.home)}</p>
+                            <p>Away: {game.full_game.moneyline.away}</p>
+                            <p>Home: {game.full_game.moneyline.home}</p>
                             <br/>
-                            <p>
-                                Over {game.full_game.over_under.total}: {sign(game.full_game.over_under.over, game.full_game.over_under.under)}
-                                {MidPoint(game.full_game.over_under.over, game.full_game.over_under.under)}
-                            </p>
-                            <p>
-                                Under {game.full_game.over_under.total}: {sign(game.full_game.over_under.under, game.full_game.over_under.over)}
-                                {MidPoint(game.full_game.over_under.over, game.full_game.over_under.under)}
-                            </p>
+                            <p>Over {game.full_game.over_under.total}: {game.full_game.over_under.over}</p>
+                            <p>Under {game.full_game.over_under.total}: {game.full_game.over_under.under}</p>
                         </div>
-                        <div className='fhMidpoints'>
-                            <h5>First Half Midpoints</h5>
-                            <p>
-                                Away: {game.first_half.spread.away_spread}
-                                ({sign(game.first_half.spread.away_odds, game.first_half.spread.home_odds)}
-                                {MidPoint(game.first_half.spread.away_odds, game.first_half.spread.home_odds)})
-                            </p>
-                            <p>
-                                Home: {game.first_half.spread.home_spread}
-                                ({sign(game.first_half.spread.home_odds, game.first_half.spread.away_odds)}
-                                {MidPoint(game.first_half.spread.away_odds, game.first_half.spread.home_odds)})</p>
-                            <br/>
-                            <p>
-                                Away: {sign(game.first_half.moneyline.away, game.first_half.moneyline.home)}
-                                {MidPoint(game.first_half.moneyline.away, game.first_half.moneyline.home)}</p>
-                            <p>
-                                Home: {sign(game.first_half.moneyline.home, game.first_half.moneyline.away)}
-                                {MidPoint(game.first_half.moneyline.away, game.first_half.moneyline.home)}</p>
-                            <br/>
-                            <p>
-                                Over {game.first_half.over_under.total}: {sign(game.first_half.over_under.over, game.first_half.over_under.under)}
-                                {MidPoint(game.first_half.over_under.over, game.first_half.over_under.under)}
-                            </p>
-                            <p>
-                                Under {game.first_half.over_under.total}: {sign(game.first_half.over_under.under, game.first_half.over_under.over)}
-                                {MidPoint(game.first_half.over_under.over, game.first_half.over_under.under)}
-                            </p>
-                        </div>
-                        <iframe 
+                        <FullGameMP
+                            game = {game}
+                        />
+                        <FirstHalfMP
+                            game = {game}
+                        />
+                        {/* <iframe 
                             className='iframe'
                             src={game.graph_address}
-                        />
+                        /> */}
                     </div>
                 </div>
             ))}
