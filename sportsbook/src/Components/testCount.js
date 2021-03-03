5e25d9bee870712dc5a3e9c1cfb69c4776e8ad7c
Aaron Williams <aaronrw4@yahoo.com>
2021-03-03 11:36:13 -0800

    Added ability to count midpoint results

--- sportsbook/src/Components/testCount.js
+++ sportsbook/src/Components/testCount.js
@@ -3,6 +3,8 @@ import {findTeam} from '../Functions/findTeam';
 import {projectedScores} from "./projectedScores";
 import {projectedSOS} from './projectedSOSscores';
 import {winResults} from '../Functions/winResults';
+import {sign} from '../Functions/sign';
+import {MidPoint} from '../Functions/midpoint';
 
 const TestCount = () => {
     let data = require('../test.json')
@@ -16,16 +18,23 @@ const TestCount = () => {
     let projNotSosCountTotal = 0
     let projSosNoBetCount = 0
     let projSosNoBetCountTotal = 0
+    let midpointCount = 0
+    let midpointTotal = 0
     
     for (let i=0; i < data.length; i++) {
         let away = findTeam(data[i].team1)
         let home = findTeam(data[i].team2)
         let projected = projectedScores(away[0], home[0])
         let projectedResults = winResults(
+            'projected',
             data[i].spread1, 
             data[i].spread2,
             data[i].score1,
             data[i].score2,
+            data[i].odds1,
+            data[i].odds2,
+            data[i].openingSpread1,
+            data[i].openingSpread2,
             projected[0].spreadAway,
             projected[0].spreadHome
         )
@@ -39,10 +48,15 @@ const TestCount = () => {
     
         let projSOS = projectedSOS(away[0], home[0])
         let projectedSosResults = winResults(
+            'projected',
             data[i].spread1, 
             data[i].spread2,
             data[i].score1,
             data[i].score2,
+            data[i].odds1,
+            data[i].odds2,
+            data[i].openingSpread1,
+            data[i].openingSpread2,
             projSOS[0].spreadAway,
             projSOS[0].spreadHome
         )
@@ -73,15 +87,46 @@ const TestCount = () => {
         } else if (projectedResults.result === 'Lose' && projectedSosResults.result === '') {
             projSosNoBetCountTotal++
         }
+
+        let midpoint1 = sign(data[i].openingOdds1, data[i].openingOdds2) + MidPoint(data[i].openingOdds1, data[i].openingOdds2)
+        let midpoint2 = sign(data[i].openingOdds2, data[i].openingOdds1) + MidPoint(data[i].openingOdds1, data[i].openingOdds2)
+    
+        let midpointResults = winResults(
+            'midpoint',
+            data[i].spread1, 
+            data[i].spread2,
+            data[i].score1,
+            data[i].score2,
+            data[i].odds1,
+            data[i].odds2,
+            midpoint1,
+            midpoint2,
+            data[i].openingSpread1,
+            data[i].openingSpread2
+        )
+        
+        if (midpointResults.result === 'Win') {
+            midpointCount++
+            midpointTotal++
+        } else if (midpointResults.result === 'Lose') {
+            midpointTotal++
+        }
     }
     
     return (
-        <div>
-            <h3>Projected Wins = {projectedCount}/{projectedTotal} ({(projectedCount/projectedTotal*100).toFixed()}%)</h3>
-            <h3>Projected SOS Wins = {projectedSosCount}/{projectedSosTotal} ({(projectedSosCount/projectedSosTotal*100).toFixed()}%)</h3>
-            <h3>Combined Wins = {combined}/{combinedTotal} ({(combined/combinedTotal*100).toFixed()}%)</h3>
-            <h3>Proj not SOS Wins = {projNotSosCount}/{projNotSosCountTotal} ({(projNotSosCount/projNotSosCountTotal*100).toFixed()}%)</h3>
-            <h3>Proj, SOS No Bet, Wins = {projSosNoBetCount}/{projSosNoBetCountTotal} ({(projSosNoBetCount/projSosNoBetCountTotal*100).toFixed()}%)</h3>
+        <div className='testCount'>
+            <div>
+                <h3>Projected Wins = {projectedCount}/{projectedTotal} ({(projectedCount/projectedTotal*100).toFixed()}%)</h3>
+                <h3>Projected SOS Wins = {projectedSosCount}/{projectedSosTotal} ({(projectedSosCount/projectedSosTotal*100).toFixed()}%)</h3>
+            </div>
+            <div>
+                <h3>Combined Wins = {combined}/{combinedTotal} ({(combined/combinedTotal*100).toFixed()}%)</h3>
+                <h3>Proj not SOS Wins = {projNotSosCount}/{projNotSosCountTotal} ({(projNotSosCount/projNotSosCountTotal*100).toFixed()}%)</h3>
+                <h3>Proj, SOS No Bet, Wins = {projSosNoBetCount}/{projSosNoBetCountTotal} ({(projSosNoBetCount/projSosNoBetCountTotal*100).toFixed()}%)</h3>
+            </div>
+            <div>
+                <h3>Midpoint Wins = {midpointCount}/{midpointTotal} ({(midpointCount/midpointTotal*100).toFixed()}%)</h3>
+            </div>
         </div>
     )
 }
