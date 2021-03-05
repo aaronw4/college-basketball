241dc155026fdcdd962161c839f559fd03710cea
Aaron Williams <aaronrw4@yahoo.com>
2021-03-05 10:39:18 -0800

    Added totals picks to bet.js

--- sportsbook/src/Components/bet.js
+++ sportsbook/src/Components/bet.js
@@ -52,20 +52,51 @@ function Bet(props) {
         props.openingSpread1,
         props.openingSpread2
     )
+    let totalPick
+    let total = Number(props.score1) + Number(props.score2)
+    let projectedTotal = Number(projected[0].total)
+    let result
+
+    if (projectedTotal > Number(props.total) + 4) {
+        totalPick = 'Over'
+    } else if (projectedTotal < Number(props.total) - 4) {
+        totalPick = 'Under'
+    } else {
+        totalPick = 'No Bet'
+    }
+    console.log(projectedTotal, Number(props.total))
+    if (totalPick === 'Over' && total > Number(props.total)) {
+        result = 'Win'
+    } else if (totalPick === 'Over' && total < Number(props.total)) {
+        result = 'Lose'
+    } else if (total === projectedTotal) {
+        result = 'Push'
+    } else if (totalPick === 'Under' && total > Number(props.total)) {
+        result = 'Lose'
+    } else if (totalPick === 'Under' && total < Number(props.total)) {
+        result = 'Win'
+    }
 
     return (
-        <div className='fgOpening bet'>
-            <h5>Projected Bet</h5>
-            <p>{projectedResults.homeAway + projectedResults.pick}</p>
-            <p>{projectedResults.result} </p>
-            <br/>
-            <h5>Projected SOS Bet</h5>
-            <p>{projectedSosResults.homeAway + projectedSosResults.pick}</p>
-            <p>{projectedSosResults.result}</p>
-            <br/>
-            <h5>MidPoint Bet</h5>
-            <p>{midpointResults.homeAway + midpointResults.pick}</p>
-            <p>{midpointResults.result}</p>
+        <div className='betContainer'>
+            <div className='fgOpening bet'>
+                <h5>Projected Bet</h5>
+                <p>{projectedResults.homeAway + projectedResults.pick}</p>
+                <p>{projectedResults.result} </p>
+                <br/>
+                <h5>Projected SOS Bet</h5>
+                <p>{projectedSosResults.homeAway + projectedSosResults.pick}</p>
+                <p>{projectedSosResults.result}</p>
+                <br/>
+                <h5>Projected Total Bet</h5>
+                <p>{totalPick}</p>
+                <p>{result}</p>
+            </div>
+            <div className='betTotals'>
+                <h5>MidPoint Bet</h5>
+                <p>{midpointResults.homeAway + midpointResults.pick}</p>
+                <p>{midpointResults.result}</p>
+            </div>
         </div>
     )
 }
